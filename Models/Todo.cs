f9a3a31e3aa829aa4560242f5859b7e0e9162a6a
cintia.oliveira <cintia.oliveeira@hotmail.com>
2021-10-18 17:37:56 -0300

    creating crud

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace todo.Models
 {
@@ -10,5 +7,6 @@ namespace todo.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public bool Done { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -5,10 +5,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using todo.Data;
 
 namespace todo
@@ -24,6 +20,8 @@ namespace todo
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
             services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
@@ -38,10 +36,9 @@ namespace todo
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
